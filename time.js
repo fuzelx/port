e3a77a8474ba5fc30761d34eb39cb8cd64030d1e
Fuzail Mansuri <104367001+fuzelx@users.noreply.github.com>
2023-08-07 20:31:47 +0530

    Create time.js

---
+++ time.js
@@ -0,0 +1,14 @@
+let currentDate = new Date();
+
+let currentTime = currentDate.getHours;
+
+if (currentTime < 12) {
+   document.getElementById("hd-p").innerHTML = "Good Morning!";
+    
+  } else if (currentTime < 18) {
+    document.getElementById("hd-p").innerHTML = "Good Afternoon!";
+    
+  } else {
+    document.getElementById("hd-p").innerHTML = "Good Night!";
+    
+  }
